bda07b67c90a1ba98bf49901abea60cfe29bd800
Joseph Ampadu <josephampadu549@gmail.com>
2022-08-08 02:58:32 +0000

    add on action for buttons and input field

--- src/main/java/io/github/daduam/StackVizController.java
+++ src/main/java/io/github/daduam/StackVizController.java
@@ -1,5 +1,67 @@
 package io.github.daduam;
 
-public class StackVizController {
+import java.util.Stack;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+// todo
+// show items in ListView
+
+public class StackVizController {
+  private Stack<String> stack;
+
+  public StackVizController() {
+    stack = new Stack<String>();
+  }
+
+  @FXML
+  private void initialize() {
+    disablePopButtonIfStackIsEmpty();
+  }
+
+  @FXML
+  private TextField stackItemTextField;
+
+  @FXML
+  private Button pushButton;
+
+  @FXML
+  private Button popButton;
+
+  @FXML
+  private void handlePushButtonAction() {
+    String newItem = stackItemTextField.getText();
+    pushItemToStack(newItem);
+    stackItemTextField.clear();
+    popButton.setDisable(false);
+  }
+
+  @FXML
+  private void handlePopButtonAction() {
+    popItemFromStack();
+    disablePopButtonIfStackIsEmpty();
+  }
+
+  private void pushItemToStack(String item) {
+    if (item.isEmpty()) {
+      return;
+    }
+    stack.push(item);
+    // update ListView
+    System.out.println(stack);
+  }
+
+  private void popItemFromStack() {
+    stack.pop();
+    // update ListView
+    System.out.println(stack);
+  }
+
+  private void disablePopButtonIfStackIsEmpty() {
+    if (stack.isEmpty()) {
+      popButton.setDisable(true);
+    }
+  }
 }
